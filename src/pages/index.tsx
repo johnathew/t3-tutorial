7b00c35d4ceb50c6aa58e758a70e308745008b6f
johnathew <jakornegay01@gmail.com>
2023-03-26 13:06:57 -0500

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
-import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
 
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -22,11 +20,12 @@ const Home: NextPage = () => {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
-          {!user.isSignedIn && <SignInButton />} {!!user.isSignedIn && <SignOutButton />}
+          {!user.isSignedIn && <SignInButton />}{" "}
+          {!!user.isSignedIn && <SignOutButton />}
         </div>
-        <p className="text-2xl text-white">
-          {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-        </p>
+        <div>
+          {data?.map((post) => (<div key={post.id}>{post.content} </div>))}
+        </div>
       </main>
     </>
   );
